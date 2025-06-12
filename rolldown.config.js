3db21358e6a053e5c3deb8adc18eda7221403714
Steve B. <184604+stevebeauge@users.noreply.github.com>
2025-06-12 18:07:22 +0200

    Minimal repro using rolldown

---
+++ rolldown.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "rolldown";
+
+export default defineConfig({
+  input: "src/index.js",
+  output: {
+    preserveModules: true,
+    dir: "dist",
+  },
+});
--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,4 +1,4 @@
 import styles from "./MyComponent.module.css";
-export const MyComponent: React.FC = () => {
+export const MyComponent = () => {
   return <p className={styles.myComponent}>Hello, World!</p>;
 };
